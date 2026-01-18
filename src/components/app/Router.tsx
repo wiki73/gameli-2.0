37edb96eba84392d18d58ab23b557eaa1d0f8252
woodemai <n.savchenkoo73@gmail.com>
2026-01-18 07:17:19 +0300

    fix bugs

--- src/components/app/Router.tsx
+++ src/components/app/Router.tsx
@@ -1,33 +1,58 @@
 import { Route, Routes } from 'react-router';
-import { AuthPage } from '@/components/pages/auth/AuthPage';
-import { DashboardPage } from '@/components/pages/dashboard/DashboardPage';
-import { MainPage } from '@/components/pages/day/MainPage';
-import { ProfilePage } from '@/components/pages/profile/ProfilePage';
-import { TaskPage } from '@/components/pages/task/TaskPage';
+import { lazy, Suspense } from 'react';
 import { ROUTES } from '@/constants/routes';
+import { FullScreenSpinner } from '../common/spinner/FullScreenSpinner';
+
+const AuthPage = lazy(() =>
+  import('@/components/pages/auth/AuthPage').then(module => ({
+    default: module.AuthPage,
+  })),
+);
+const DashboardPage = lazy(() =>
+  import('@/components/pages/dashboard/DashboardPage').then(module => ({
+    default: module.DashboardPage,
+  })),
+);
+const MainPage = lazy(() =>
+  import('@/components/pages/day/MainPage').then(module => ({
+    default: module.MainPage,
+  })),
+);
+const ProfilePage = lazy(() =>
+  import('@/components/pages/profile/ProfilePage').then(module => ({
+    default: module.ProfilePage,
+  })),
+);
+const TaskPage = lazy(() =>
+  import('@/components/pages/task/TaskPage').then(module => ({
+    default: module.TaskPage,
+  })),
+);
 
 export const Router = () => (
-  <Routes>
-    <Route
-      element={<MainPage />}
-      path={ROUTES.MAIN}
-    />
-    <Route
-      element={<AuthPage />}
-      path={ROUTES.AUTH}
-    />
-    <Route
-      element={<ProfilePage />}
-      path={ROUTES.PROFILE}
-    />
+  <Suspense fallback={<FullScreenSpinner />}>
+    <Routes>
+      <Route
+        element={<MainPage />}
+        path={ROUTES.MAIN}
+      />
+      <Route
+        element={<AuthPage />}
+        path={ROUTES.AUTH}
+      />
+      <Route
+        element={<ProfilePage />}
+        path={ROUTES.PROFILE}
+      />
 
-    <Route
-      element={<DashboardPage />}
-      path={ROUTES.DASHBOARD}
-    />
-    <Route
-      element={<TaskPage />}
-      path={ROUTES.TASK}
-    />
-  </Routes>
+      <Route
+        element={<DashboardPage />}
+        path={ROUTES.DASHBOARD}
+      />
+      <Route
+        element={<TaskPage />}
+        path={ROUTES.TASK}
+      />
+    </Routes>
+  </Suspense>
 );
